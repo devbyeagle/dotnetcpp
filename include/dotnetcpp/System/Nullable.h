e915b7922feaa96ba751c5b0471146ad8e5eb39b
DevByEagle <142937112+devbyeagle@users.noreply.github.com>
2026-03-26 19:34:58 -0400

    Update Nullable.h

--- include/dotnetcpp/System/Nullable.h
+++ include/dotnetcpp/System/Nullable.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <type_traits>
+#include <utility>
 
 namespace System
-{
+{    
     template<typename T>
     struct Nullable
     {
@@ -15,16 +16,35 @@ namespace System
         constexpr Nullable() noexcept { }
         constexpr Nullable(std::nullptr_t) noexcept { }
 
-        template<typename U = typename std::remove_cv<T>::type>
-        constexpr Nullable(U&& value);
+        template<typename U = T,
+                 typename = std::enable_if_t<!std::is_same_v<std::decay_t<U>, Nullable>>>
+        constexpr Nullable(U&& value) : hasValue(true), value(std::forward<U>(value)) { }
 
-        constexpr bool HasValue() const noexcept;
-        const T& Value() const;
+        constexpr bool HasValue() const noexcept { return hasValue; }
 
-        constexpr T GetValueOrDefault() const;
-        constexpr T GetValueOrDefault(const T& defaultValue) const;
+        constexpr T& Value() &
+        {
+            if (!hasValue)
+            {
+                throw;
+            }
+            return value;
+        }
+
+        constexpr T GetValueOrDefault() const& { return value; }
+        constexpr T GetValueOrDefault(T defaultValue) { return hasValue ? value : defaultValue; }
 
         explicit operator T() const { return Value(); }
-        friend bool operator==(const Nullable& n1, const Nullable& n2);
+
+        friend bool operator==(const Nullable<T>& n1, const Nullable<T>& n2)
+        {
+            if (n1.HasValue())
+            {
+                if (n2.HasValue()) return n1.Value() == n2.Value();
+                return false;
+            }
+            if (n2.HasValue()) return false;
+            return true;
+        }
     };
 }
